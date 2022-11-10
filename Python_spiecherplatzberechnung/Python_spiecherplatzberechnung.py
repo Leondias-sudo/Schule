c3f8545edbd345db1dff3f3ef0c2899d4242b9e7
leon meihaus <leon.meihaus@amcon.de>
2022-11-10 14:35:53 +0100

    Repo update

--- Python_spiecherplatzberechnung/Python_spiecherplatzberechnung.py
+++ Python_spiecherplatzberechnung/Python_spiecherplatzberechnung.py
@@ -2,6 +2,6 @@ Samplingrate = int(input())
 Audiolengt = int(input()) 
 Anzahlton = int(input()) 
 Samplingtiefe = int(input()) 
-Ergebnis = Samplingrate * Audiolengt * Anzahlton * Samplingtiefe 
+Ergebnis = Samplingrate * Audiolengt * Anzahlton * Samplingtiefe #Hier  
 Ergebnis/8000 #Der Output ist in Bytes
 print(Ergebnis)
