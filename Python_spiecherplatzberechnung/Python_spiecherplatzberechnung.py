e580e8d1dd22ac185904dc9ea1d4eb470167be5a
leon meihaus <leon.meihaus@amcon.de>
2022-11-16 10:57:23 +0100

    Update Repo

--- Python_spiecherplatzberechnung/Python_spiecherplatzberechnung.py
+++ Python_spiecherplatzberechnung/Python_spiecherplatzberechnung.py
@@ -1,7 +1,53 @@
-Samplingrate = int(input()) 
-Audiolengt = int(input()) 
-Anzahlton = int(input()) 
-Samplingtiefe = int(input()) 
-Ergebnis = Samplingrate * Audiolengt * Anzahlton * Samplingtiefe #Hier  
-Ergebnis/8000 #Der Output ist in Bytes
-print(Ergebnis)
+Samplingrate = float(input("Samplingrate: ")) 
+Audiolengt = float(input("Audiolänge: ")) 
+Anzahlton = float(input("Anzahlton: ")) 
+Samplingtiefe = float(input("Samplingtiefe: "))
+Einheit = input("Einheit: ") 
+Ergebnis = Samplingrate * Audiolengt * Anzahlton * Samplingtiefe 
+float(Ergebnis)
+teilwert = 1000
+teilwertbyte = 8
+float(teilwertbyte)
+float(teilwert)
+if Einheit != "":
+    if Einheit == "Bit":
+        print(Ergebnis,"Bit")
+
+    Ergebnis=Ergebnis/teilwertbyte
+    if Einheit == "Btyte":
+        print(Ergebnis,"Byte")
+
+    Ergebnis=Ergebnis/teilwert
+
+    if Einheit == "Kilobyte":
+        print(Ergebnis,"Kb")
+
+    Ergebnis=Ergebnis/teilwert
+
+    if Einheit == "Megabyte":
+        print(Ergebnis,"Mb")
+
+    Ergebnis=Ergebnis/teilwert
+
+    if Einheit == "Gigabyte":
+        print(Ergebnis,"Gb")
+
+    Ergebnis=Ergebnis/teilwert
+
+    if Einheit =="Terabyte":
+        print(Ergebnis,"Tb")
+else:
+    if Ergebnis <= teilwert:
+        print(Ergebnis,"Bit")
+
+    Ergebnis=Ergebnis/teilwertbyte
+
+    if Ergebnis<= teilwert:
+        print(Ergebnis,"Byte")
+
+    while Ergebnis >= teilwert:
+        Ergebnis = Ergebnis/teilwertbyte
+        if Ergebnis <= teilwert:
+            print(Ergebnis)
+            break
+        
