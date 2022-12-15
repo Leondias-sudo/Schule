2ddfab8c465b071d1c857d976a5bff1ef533a8fc
Leon Meihaus <leon.meihaus@amcon.de>
2022-12-15 14:51:56 +0100

    added file

---
+++ Programieren/Python/LF5/Verschluesselung/Verschluesselung.py
@@ -0,0 +1,19 @@
+Zahl = 50
+i=1
+while i != Zahl:
+    if Zahl<=1:
+        print(f"{Zahl} ist keine Primzahl")
+        break
+
+    if Zahl == 2:
+        print(f"{Zahl} ist keine Primzahl")
+        break
+
+    if i%2 == 0:
+        print(f"{i} ist keine Primzahl") 
+    else:
+        print(f"{i} ist eine Primzahl")
+
+    i=i+1
+
+    
